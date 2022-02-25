28ff4ebc980895e8894ed54caf0b7d8d70c8e97c
liutao.me <liutao.me@icloud.com>
2022-02-25 20:45:12 +0800

    demo fabric

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# react-ct1ext
+# meeting room map
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ct1ext)
--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import React from "react";
+import React, {useState, useEffect} from 'react';
 import "./style.css";
+import {fabric} from "fabric";
+
 
 export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+
+    const [canvas, setCanvas] = useState('');
+
+
+    const initCanvas = () => (
+        new fabric.Canvas('canvas', {
+            height: 800,
+            width: 800,
+            backgroundColor: 'pink'
+        })
+    );
+
+    useEffect(() => {
+        setCanvas(initCanvas());
+    }, []);
+
+    const onClick = () => {
+        var canvas = new fabric.Canvas('c');
+
+        console.log(1);
+// create a rectangle object
+        var rect = new fabric.Rect({
+            left: 100,
+            top: 100,
+            fill: 'red',
+            width: 20,
+            height: 20
+        });
+
+// "add" rectangle onto canvas
+        canvas.add(rect);
+
+    }
+
+    return (
+        <div>
+            <div onClick={onClick}>Hello !</div>
+            <canvas id="c"></canvas>
+            <canvas id="canvas"/>
+
+            <p>Start editing to see some magic happen :)</p>
+        </div>
+    );
 }
